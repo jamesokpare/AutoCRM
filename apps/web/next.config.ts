a37eb20239da2250a23273894cff31409bb62dcc
daveokpare <okparedave@gmail.com>
2026-05-24 23:18:43 +0100

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@crm-tool/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
